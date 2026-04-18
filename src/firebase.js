8cbb3799e1fb1e2f2f23d2671fb87a980e5267b2
chinamyeon <adia902222@gmail.com>
2026-04-19 02:37:53 +0900

    fix: shop 원본 복원 + firebase authDomain/ads.txt pub ID 수정

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "archiview.store",
+    authDomain: "archiview.shop",
     projectId: "book-site-123",
     storageBucket: "book-site-123.firebasestorage.app",
     messagingSenderId: "176157090689",
